dd562dd11cd3312517a9c40e1e2dfe4a9811aab7
Amanda-Ng <89530159+Amanda-Ng@users.noreply.github.com>
2024-02-27 04:41:26 +0000

    commit

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -48,7 +48,7 @@ Image *load_ppm(const char *filename) {
     FILE *file = fopen(filename, "r");
 
     int width, height, max_val;
-    fscanf(file, "%d %d %d", &width, &height, &max_val);
+    fscanf(file, "%*s %*d %d %d %d", &width, &height, &max_val);
 
     // Allocate memory for image
     Image *image = (Image *)malloc(sizeof(Image));
@@ -74,7 +74,7 @@ Image *load_sbu(const char *filename) {
     FILE *file = fopen(filename, "r");    
 
     int width, height, entries;
-    fscanf(file, "%d %d %d", &width, &height, &entries);
+    fscanf(file, "%*s %d %d %d", &width, &height, &entries);
     unsigned char colors[entries][3];
 
     // Allocate memory for image
@@ -91,14 +91,33 @@ Image *load_sbu(const char *filename) {
         fscanf(file, "%hhu %hhu %hhu", &colors[i][0], &colors[i][1], &colors[i][2]);
     }
 
-    // Read pixel data
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            int index=0;
+    // Read pixel data with run-length encoding
+    int pixelIndex = 0;
+    int x = 0, y = 0;
+    while (y < height) {
+        int count, index;
+        fscanf(file, "%d", &index);
+
+        if (index <= 0) {
+            // RLE encoding
+            fscanf(file, "%d", &count);
             fscanf(file, "%d", &index);
-            image->pixels[i][j].red=colors[index][0];
-            image->pixels[i][j].green=colors[index][1];
-            image->pixels[i][j].blue=colors[index][2];
+        } else {
+            // Single pixel
+            count = 1;
+        }
+
+        for (int i = 0; i < count; i++) {
+            image->pixels[y][x].red = colors[index][0];
+            image->pixels[y][x].green = colors[index][1];
+            image->pixels[y][x].blue = colors[index][2];
+
+            pixelIndex++;
+            x++;
+            if (x == width) {
+                x = 0;
+                y++;
+            }
         }
     }
 
@@ -199,7 +218,6 @@ int main(int argc, char **argv) {
 
     // return 1000;
 
-    // Define variables to store command-line arguments
     char *input_file = NULL;
     char *output_file = NULL;
     char *c_args = NULL;
@@ -213,7 +231,6 @@ int main(int argc, char **argv) {
     int p_argument_count = 0;
     int r_argument_count = 0;
 
-    // Process command-line arguments using getopt
     int opt;
     while ((opt = getopt(argc, argv, ":i:o:c:p:r:")) != -1) {
         switch (opt) {
@@ -274,19 +291,16 @@ int main(int argc, char **argv) {
         }
     }
 
-    // Check for missing required arguments
     if (input_file == NULL || output_file == NULL) {
         fprintf(stderr, "Missing argument\n");
         return MISSING_ARGUMENT;
     }
 
-    // Check for duplicate arguments
     if (i_argument_count > 1 || o_argument_count > 1 || c_argument_count > 1 || p_argument_count > 1 || r_argument_count > 1) {
         fprintf(stderr, "Duplicate argument\n");
         return DUPLICATE_ARGUMENT;
     }
 
-    // Check if input file exists
     FILE *test_output = fopen(input_file, "r");
     if (!fileExists(input_file) || test_output==NULL) {
         fprintf(stderr, "Input file missing\n");
@@ -294,7 +308,6 @@ int main(int argc, char **argv) {
     }
     fclose(test_output);
 
-    // Check if output file is writable
     FILE *output_test = fopen(output_file, "w");
     if (output_test == NULL) {
         fprintf(stderr, "Output file unwritable\n");
@@ -322,7 +335,6 @@ int main(int argc, char **argv) {
     }
     fclose(font_test);
     
-    // If all checks pass, return 0 indicating success
     return 0;
 
     // Image *image=NULL;
